c596a73dd325829b29c6248ed1bc746b6d190976
Praphulla28 <praphulla.mahendra@incture.com>
2020-06-26 17:11:01 +0530

    demolib3

--- app/src/main/java/com/example/logutilsexample/MainActivity.kt
+++ app/src/main/java/com/example/logutilsexample/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.logutilsexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.demolib3.LogUtil3
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
---
+++ demolib3/src/main/java/com/example/demolib3/LogUtil3.java
@@ -0,0 +1,10 @@
+package com.example.demolib3;
+
+import android.util.Log;
+
+public class LogUtil3 {
+
+    public static void showLog2(String demo){
+        Log.d("LogUtils",demo);
+    }
+}
